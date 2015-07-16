3adebcb2dbf84bb687e7b9e81ef9bbebdd039bff
Aditya Chowdhry <adityachowdhry09@gmail.com>
2015-07-16 16:44:54 +0530

    Added seed data

--- db/migrate/20150712085026_add_role_to_admin_users.rb
+++ db/migrate/20150712085026_add_role_to_admin_users.rb
@@ -1,5 +1,5 @@
 class AddRoleToAdminUsers < ActiveRecord::Migration
   def change
-    add_column :admin_users, :role, :string
+    add_column :admin_users, :role, :string ,:default=> 'admin'
   end
 end
